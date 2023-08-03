308e6e8a24ca37db55df717e6a52e41740db046f
musaffarulislam <musaffarulislam@gmail.com>
2023-08-03 16:21:44 +0530

    signu up error 2

--- src/interfaces/persistence/auth.repository.impl.ts
+++ src/interfaces/persistence/auth.repository.impl.ts
@@ -4,6 +4,7 @@ import { Auth } from "../../app/entity/auth.entity";
 export class AuthRepositoryImpl {
   public async createUser(username: string, email: string, phoneNumber: string, role: string, password: string): Promise<void> {
     try {
+      console.log("username :",username, "email : ",email, "phoneNumber : ",phoneNumber, "role : ",role," password : ",password)
       const auth = new AuthModel({ username, email, phoneNumber, role, password });
       await auth.save();
     } catch (error) {
